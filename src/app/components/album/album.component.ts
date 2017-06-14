f9ba46e08fb6cc7ff330346fef6ecc39c2f48d31
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-06-13 18:38:55 -0600

    added album component

---
+++ src/app/components/album/album.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Album } from '../../../Album';
+import { SpotifyService } from '../../services/spotify.service';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: 'album.component.html'
+})
+export class AlbumComponent implements OnInit{
+  id: string;
+  album: Album;
+
+  constructor(private service: SpotifyService, private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    this.route.params.map(params => params['id'])
+      .subscribe((id) => {
+        this.service.getAlbum(id)
+          .subscribe(album => {
+            this.album = album;
+          })
+      })
+  }
+}
