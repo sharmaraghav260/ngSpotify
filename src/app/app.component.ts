dfd87a5c044e00217f94d12381daee53838d2127
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-06-13 19:32:35 -0600

    navbar fails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,5 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'my-app',
   templateUrl: 'app.component.html',
-  //styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
